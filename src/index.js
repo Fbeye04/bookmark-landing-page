7d1b08c874954fe159baf84ce5d0ba2cd961f453
Fachrezi <107632872+Fbeye04@users.noreply.github.com>
2025-06-14 12:15:45 +0700

    complete the feature of moving between tabs

--- src/index.js
+++ src/index.js
@@ -4,6 +4,35 @@ const closeButton = document.getElementById("close-button");
 const titleMenuClose = document.getElementById("title-menu-close");
 const body = document.body;
 
+const tabData = [
+  {
+    id: 1,
+    imageSrc: "./images/illustration-features-tab-1.svg",
+    heading: "Bookmark in one click",
+    paragraph:
+      "Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favorite sites.",
+  },
+  {
+    id: 2,
+    imageSrc: "./images/illustration-features-tab-2.svg",
+    heading: "Intelligent search",
+    paragraph:
+      "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.",
+  },
+  {
+    id: 3,
+    imageSrc: "./images/illustration-features-tab-3.svg",
+    heading: "Share your bookmarks",
+    paragraph:
+      "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.",
+  },
+];
+
+const tabButtons = document.querySelectorAll(".tab-button");
+const featureImage = document.getElementById("feature-image");
+const featureHeading = document.getElementById("feature-heading");
+const featureParagraph = document.getElementById("feature-paragraph");
+
 menuButton.addEventListener("click", () => {
   navMenu.classList.remove("hidden");
   navMenu.classList.add("flex");
@@ -22,4 +51,32 @@ closeButton.addEventListener("click", () => {
   closeButton.classList.add("hidden");
   closeButton.classList.remove("flex");
   titleMenuClose.classList.remove("hidden");
+});
+
+const handleTabClick = (event) => {
+  // set active button
+  tabButtons.forEach((button) => {
+    button.classList.remove("tab-active");
+  });
+  const clickedButton = event.currentTarget;
+  clickedButton.classList.add("tab-active");
+
+  // change feature content
+  const targetTabNumber = clickedButton.dataset.tab;
+
+  // find the tab data based on the clicked button
+  const contentToShow = tabData.find(
+    (data) => data.id === Number(targetTabNumber)
+  );
+
+  // if data is found, update the content
+  if (contentToShow) {
+    featureImage.src = contentToShow.imageSrc;
+    featureHeading.textContent = contentToShow.heading;
+    featureParagraph.textContent = contentToShow.paragraph;
+  }
+};
+
+tabButtons.forEach((button) => {
+  button.addEventListener("click", handleTabClick);
 });
